43fe1d94fc7feff6662f3d137476c77cd5969cda
Angie Badillo <hunnybunny@live.com.mx>
2018-08-08 18:37:14 -0700

    Day2 Solution If-Else

--- Day2-Conditional Statements-If-Else.js
+++ Day2-Conditional Statements-If-Else.js
@@ -6,26 +6,21 @@
  **/
 
 function getGrade(score) {
-    var grade;
+    let grade;
+
     // Write your code here
-    if (25 < score && score <= 30){
-        grade = ("A");
-    }
-    if (20 < score && score <= 25){
-        grade = ("B");
-    }
-    if (15 < score && score <= 20){
-        grade = ("C");
-    }
-    if (10 < score && score <= 15){
-        grade = ("D");
-    }
-    if (5 < score && score <= 10){
-        grade = ("E");
-    }
-    if (0 < score && score <= 5){
-        grade = ("F");
+    if (score > 25) {
+        grade = 'A';
+    } else if (score > 20 && score <= 25) {
+        grade = 'B';
+    } else if (score > 15 && score <= 20) {
+        grade = 'C';
+    } else if (score > 10 && score <= 15) {
+        grade = 'D';
+    } else if (score > 5 && score <= 10) {
+        grade = 'E';
+    } else {
+        grade = 'F';
     }
 
     return grade;
-}
